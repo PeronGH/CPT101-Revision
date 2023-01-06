3ea543a570e2c520b604d1b1f8db6292871aa24d
peron <peron_ap@icloud.com>
2023-01-07 00:25:45 +0800

    add radix convertor

---
+++ radix_convertor.ts
@@ -0,0 +1,30 @@
+const radixMap = new Map<string, number>([
+    ['b', 2],
+    ['o', 8],
+    ['d', 10],
+    ['h', 16],
+]);
+
+while (true) {
+    const input = prompt('Enter a number to convert:');
+
+    if (input === null) break;
+
+    console.log(getResult(input));
+}
+
+function getResult(input: string): string {
+    const radix = radixMap.get(input[0]);
+
+    const inputNumber = parseInt(
+        radix === undefined ? input : input.slice(1),
+        radix
+    );
+
+    return ''.concat(
+        `Binary: ${inputNumber.toString(2)}\n`,
+        `Octal: ${inputNumber.toString(8)}\n`,
+        `Decimal: ${inputNumber.toString(10)}\n`,
+        `Hexadecimal: ${inputNumber.toString(16).toUpperCase()}\n`
+    );
+}
